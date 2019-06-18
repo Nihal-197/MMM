fc457589e1f8ff4eae7fa00400542d7f93573378
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-18 15:50:30 +0530

    Added Correlation feature

--- mmm_pre_all.py
+++ mmm_pre_all.py
@@ -34,6 +34,46 @@ def col_drop(hier,hier_list):
             col_2_drop=hier_list[j:]
         
     return col_2_drop 
+
+#function to make the dict with the channel_values 
+def str_fin_ad(string):
+    if bool(re.search('ad_stock_nad_(\w+)_log',string)):
+        return re.findall('ad_stock_nad_(\w+)_log',string)[0]
+    elif bool(re.search('(\w+)_log',string)):
+        return re.findall('(\w+)_log',string)[0]
+#create a for loop to get the dictionary for the variables and then transfom them into adstock and log 
+def new_map_dict(corr):
+    map_dict = {}
+    for i in range(len(corr.columns)-1):
+        string = list(corr.columns)[i]
+        map_dict[str_fin_ad(string)]=string
+        
+    return {y:x for x,y in map_dict.items()}
+#CREATE A FUCNTION BY TAKING THE VALUES OF THE DICT RETURN BY THE CORRELATION AND ADD THOSE PURE VALUES, LATER TRNASFORM THEM AND REPEAT  UNTIL WE FIND NO CORRELATION
+
+def corr_find(data_promo1,channel_list,hier,spc_hier,cor_coef_ad,cor_coef_else):
+    non_promo_col=np.array(['Price','PCV'])
+    chann=np.array(channel_list)
+    driver = [str("ad_stock_nad_")+i for i in chann.astype('object')+ "_log"]+[j for j in non_promo_col.astype('object') + "_log"]
+    lis1=driver+['Sales_log']
+    corr1= data_promo1[data_promo1[hier]==spc_hier][lis1].corr()
+    corr_find.corr = corr1.copy()
+    cor1_dict={}
+    #creating a dictionary mapping elements with correlation
+    for i in range(len(corr1)-1):
+        if i<len(chann):
+            for j in range(len(corr1)-1):        
+                if ((corr1[lis1[i]].iloc[j])>cor_coef_ad and i>j):
+                    #print(f'For NAD ad stock ','Correlation is >{cor_coef_ad} between',lis1[i],'and',corr1[lis1[i]].index[j])
+                    cor1_dict[f'{lis1[i]}']=f'{corr1[lis1[i]].index[j]}'
+        else:
+            for j in range(len(corr1)-1):        
+                if ((corr1[lis1[i]].iloc[j])>cor_coef_else and i>j):
+                    #print(f'For NAD ad stock ','Correlation is >{cor_coef_ad} between',lis1[i],'and',corr1[lis1[i]].index[j])
+                    cor1_dict[f'{lis1[i]}']=f'{corr1[lis1[i]].index[j]}'
+                    raise ValueError("Correlation found! Model discarded")
+    return cor1_dict
+
 #Now some values can contain 0 values for log function hence finding index of them and replacing them with the second
 #min value
 def rep_zero(data,var):
