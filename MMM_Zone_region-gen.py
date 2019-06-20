6220d60dce56cee07e831b72a6926dafb4c28cb0
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-20 14:31:52 +0530

    Minor update in input format (JSON)

--- MMM_Zone_region-gen.py
+++ MMM_Zone_region-gen.py
@@ -68,6 +68,32 @@ def check_request():
 	except Exception as e:
 		return json_response({"status_code":"400", "status_txt":'BAD REQUEST ' + str(e.__class__.__name__)}, status=400)
 
+# =============================================================================
+# def get_data(eql_generated, api_key=None, api_secret=None, limit=None):
+#     """ Get the needed data from the API """
+#  
+#     data_payload = {
+#     'api_key': api_key,
+#     'api_secret': api_secret, 
+#     'data': {eql: eql_generated}
+#         }
+#  
+#     data_header = { 
+#     'content-type': 'application/json'
+#     }
+#     response = requests.request("POST", url=config.EQL_PROCESSOR_URL, json=data_payload, headers=data_header)
+#     data = response.json()
+#      
+#     if data['status_code'] != 200:
+#         raise ValueError(data["status_txt"])
+#     if not data['data']:
+#         raise ValueError(data["status_txt"])
+#     if 'status_code' in data['data']:
+#         raise ValueError(data['data']["status_txt"])
+#          
+#     return data
+# =============================================================================
+
 
 @app.after_request
 def check_response(response):
@@ -92,10 +118,10 @@ def check_response(response):
         "Radio": 0.3, 
         "TV": 1.2,
         "PCV": 30, 
-        "Price": 500
+        "Price": 0.5,
+        'Budget' : 5
         
     }
-
 
 @app.route('/mixed_models/', methods=['POST'])
 def mmm_api():
