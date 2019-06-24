0ee841e50a89474ac4e9579fb253353974fb40b6
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-24 16:25:34 +0530

    minor changes in corr_merge and a new fun added

--- corr_finder.py
+++ corr_finder.py
@@ -2,13 +2,20 @@ import numpy as np
 import re 
 from collections import deque
 from mmm_pre_zone import *
+from mmm_pre_all import *
 
 #function to make the dict with the channel_values 
-def str_fin_ad(string):
+def str_fin_ad_log(string):
     if bool(re.search('ad_stock_nad_(\w+)_log',string)):
         return re.findall('ad_stock_nad_(\w+)_log',string)[0]
     elif bool(re.search('(\w+)_log',string)):
         return re.findall('(\w+)_log',string)[0]
+
+def str_fin_ad(string):
+    if bool(re.search('ad_stock_nad_(\w+)',string)):
+        return re.findall('ad_stock_nad_(\w+)',string)[0]
+    elif bool(re.search('(\w+)',string)):
+        return re.findall('(\w+)',string)[0]
     
 #create a for loop to get the dictionary for the variables and then transfom them into adstock and log 
 def new_map_dict(corr):
@@ -20,14 +27,17 @@ def new_map_dict(corr):
     return {y:x for x,y in map_dict.items()}
 
 #CREATE A FUCNTION BY TAKING THE VALUES OF THE DICT RETURN BY THE CORRELATION AND ADD THOSE PURE VALUES, 
-#LATER TRNASFORM THEM AND REPEAT  UNTIL WE FIND NO CORRELATION
+#LATER TRNASFORM THEM AND REPEAT UNTIL WE FIND NO CORRELATION
 
 
 def corr_find(data_promo1,channel_list,cor_coef_ad,cor_coef_else):
     non_promo_col=np.array(['Price','PCV'])
     chann=np.array(channel_list)
-    driver = [str("ad_stock_nad_")+i for i in chann.astype('object')+ "_log"]+[j for j in non_promo_col.astype('object') + "_log"]
-    lis1=driver+['Sales_log']
+    #driver = [str("ad_stock_nad_")+i for i in chann.astype('object')+ "_log"]+[j for j in non_promo_col.astype('object') + "_log"]+ list(range(4))
+    driver = [str("ad_stock_nad_")+i for i in chann.astype('object')]+[j for j in non_promo_col.astype('object')] 
+
+    #lis1=driver+['Sales_log']
+    lis1=driver+['Sales']
     corr1= data_promo1[lis1].corr()
     corr_find.corr = corr1.copy()
     cor1_dict={}
@@ -50,7 +60,8 @@ def corr_find(data_promo1,channel_list,cor_coef_ad,cor_coef_else):
 #new map dict  
 #CHANNEL_LIST IS SUBJECTED TO CHANGE 
 #CHANGE lr to lr_list and same for decay
-added_col1=deque([])
+
+added_col1=deque([]) 
 added_col2=deque([])
 
 def corr_merge(data_promo,data_promo1,hier,spc_hier,zone_reg_col,channel_list,spc_hier_list,mapped,cor1_dict,cor_coef_ad,cor_coef_else,lr,decay):
@@ -62,22 +73,56 @@ def corr_merge(data_promo,data_promo1,hier,spc_hier,zone_reg_col,channel_list,sp
         added_col1.append(item_1)
         added_col2.append(item_2)
         data_promo1[item_1]=data_promo1[item_1]+data_promo1[item_2]
-        data_promo1.drop(columns=[list(cor1_dict.keys())[0],list(cor1_dict.values())[0],f'ad_stock_s_{item_2}_log',f'ad_stock_s_{item_1}_log'],inplace=True)
+        #data_promo1.drop(columns=[list(cor1_dict.keys())[0],list(cor1_dict.values())[0],f'ad_stock_s_{item_2}_log',f'ad_stock_s_{item_1}_log'],inplace=True)
+        data_promo1.drop(columns=[list(cor1_dict.keys())[0],list(cor1_dict.values())[0],f'ad_stock_s_{item_2}',f'ad_stock_s_{item_1}'],inplace=True)
+
         #print(item_1)  data,data_promo,var,hier,spc_hier,zone_reg_col,lr_list,decay_list 
         for j in spc_hier_list: 
             ad_stock_s_curve_u(data_promo1,data_promo,item_1,hier,j,zone_reg_col,lr,decay)
-            log_var_crores(data_promo1,str('ad_stock_nad_')+item_1)
-            log_var_crores(data_promo1,str('ad_stock_s_')+item_1)
-        cor1_dict.pop(list(cor1_dict.keys())[0])
+# =============================================================================
+#             log_var_crores(data_promo1,str('ad_stock_nad_')+item_1)
+#             log_var_crores(data_promo1,str('ad_stock_s_')+item_1)
+# =============================================================================
+        cor1_dict.pop(list(cor1_dict.keys())[0]) 
         channel_list = list(set(channel_list)- set([item_2]))
         print(item_2)
         print(channel_list)
         cor1_dict = corr_find(data_promo1,channel_list,cor_coef_ad,cor_coef_else)
         print('CORRRELATION DICT',cor1_dict)
         return corr_merge(data_promo,data_promo1,hier,spc_hier,zone_reg_col,channel_list,spc_hier_list,mapped,cor1_dict,cor_coef_ad,cor_coef_else,lr,decay)
-    else :
+    else : 
         return data_promo1,channel_list,added_col1,added_col2
 
+def corr_merge_all(data_promo1,hier,spc_hier,channel_list,spc_hier_list,mapped,cor1_dict,cor_coef_ad,cor_coef_else,lr,decay):
+    
+    if cor1_dict: 
+        #print(cor1_dict)
+        item_1= mapped[list(cor1_dict.keys())[0]]
+        item_2 = mapped[list(cor1_dict.values())[0]]
+        added_col1.append(item_1)
+        added_col2.append(item_2)
+        data_promo1[item_1]=data_promo1[item_1]+data_promo1[item_2]
+        #data_promo1.drop(columns=[list(cor1_dict.keys())[0],list(cor1_dict.values())[0],f'ad_stock_s_{item_2}_log',f'ad_stock_s_{item_1}_log'],inplace=True)
+        data_promo1.drop(columns=[list(cor1_dict.keys())[0],list(cor1_dict.values())[0],f'ad_stock_s_{item_2}',f'ad_stock_s_{item_1}'],inplace=True)
+
+        #print(item_1)  data,data_promo,var,hier,spc_hier,zone_reg_col,lr_list,decay_list 
+        for j in spc_hier_list: 
+            ad_stock_s_curve_u(data_promo1,item_1,hier,j,lr,decay)
+# =============================================================================
+#             log_var_crores(data_promo1,str('ad_stock_nad_')+item_1)
+#             log_var_crores(data_promo1,str('ad_stock_s_')+item_1)
+# =============================================================================
+        cor1_dict.pop(list(cor1_dict.keys())[0]) 
+        channel_list = list(set(channel_list)- set([item_2]))
+        print(item_2)
+        print(channel_list)
+        cor1_dict = corr_find(data_promo1,channel_list,cor_coef_ad,cor_coef_else)
+        print('CORRRELATION DICT',cor1_dict)
+        return corr_merge(data_promo1,hier,spc_hier,channel_list,spc_hier_list,mapped,cor1_dict,cor_coef_ad,cor_coef_else,lr,decay)
+    else : 
+        return data_promo1,channel_list,added_col1,added_col2
+    
+    
 #For making similar changes in the user input as we did in correlation df 
 def post_col_chg(user_input,added_col1,added_col2):
     if added_col1:
