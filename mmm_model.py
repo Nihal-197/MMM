c883ba880ecf030d154078e618fa1a9d9eb50521
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-24 17:45:38 +0530

    added accuracy

--- mmm_model.py
+++ mmm_model.py
@@ -1,8 +1,7 @@
-
 import numpy as np
 from math import sqrt
 import statsmodels.formula.api as smf
-from sklearn.metrics import r2_score
+from sklearn.metrics import r2_score,mean_squared_error
 #===========================MODEL=============================
 def Model(data_promo1,hier,channel_list):
     non_promo_col=np.array(['Price','PCV'])
@@ -20,12 +19,14 @@ def Model(data_promo1,hier,channel_list):
     eqn1=' ~ '+' + '.join([i for i in Model.driver1])
     md1=smf.mixedlm('Sales' + eqn1,data = data_promo1,groups=data_promo1[hier],re_formula=eqn1)
     Model.mdf1=md1.fit(method='lbfgs')
+    #mdf= Model.mdf1
     
     #MODEL WITH SEASONALITY 
     Model.driver1_sea = [str("ad_stock_nad_")+i for i in chann.astype('object')]+[j for j in non_promo_col.astype('object')]+[str('season_')+str(i) for i in range(4)]
     eqn1_sea=' ~ '+' + '.join([i for i in Model.driver1_sea])
     md1_sea=smf.mixedlm('Sales' + eqn1_sea,data = data_promo1,groups=data_promo1[hier],re_formula=eqn1_sea)
     Model.mdf1_sea=md1_sea.fit(method='lbfgs')
+    #mdf1_sea = Model.mdf1_sea
     
 # =============================================================================
 #     #Model 2 with S curve 
@@ -38,10 +39,13 @@ def Model(data_promo1,hier,channel_list):
     Model.driver=Model.driver1
     Model.mdf=Model.mdf1
     
+    rms1 = r2_score(data_promo1['Sales'],Model.mdf1.fittedvalues)
+    rms2 = r2_score(data_promo1['Sales'],Model.mdf1_sea.fittedvalues)
+
 # =============================================================================
 #     
-#     acc1 = r2_score(data_promo1['Sales_log'],Model.mdf1.fittedvalues)
-#     acc2 = r2_score(data_promo1['Sales_log'],Model.mdf2.fittedvalues)
+#     acc1 = r2_score(data_promo1['Sales'],Model.mdf1.fittedvalues)
+#     acc1 = r2_score(data_promo1['Sales_log'],Model.mdf1_sea.fittedvalues)
 #     #if time (choose the best model)
 #     
 #     if acc1>acc2:
