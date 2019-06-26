85b1545a8399f6b90a627f711c6f361d4e0d1746
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-26 11:54:08 +0530

    Changes in rounding off

--- mmm_post_pro.py
+++ mmm_post_pro.py
@@ -23,19 +23,23 @@ def col_drop(hier,hier_list):
 #FUNCTION TO SUM(COLUMN OF data)*COEF/ SALES
 def vol_distr(data_promo1,hier,spc_hier,chann_list,coeff_1_user):
     t_sales= data_promo1[data_promo1[hier]==spc_hier]['Sales'].sum()
-    vol= {}
-    #6 is added because 4=dummy_var and 2 = PCV and PRICE
+    vol= {} 
     for i in chann_list:
-        vol[chann_list]= data_promo1[data_promo1[hier]==spc_hier][i].sum()*coeff_1_user[i]/t_sales
-    for i in range(4):
-        vol[str('season_')+i]=data_promo1[data_promo1[hier]==spc_hier][i].sum()*coeff_1_user[i]/t_sales
-    vol['Price']=data_promo1[data_promo1[hier]==spc_hier]['Price'].sum()*coeff_1_user['Price']/t_sales
-    vol['PCV']=data_promo1[data_promo1[hier]==spc_hier]['PCV'].sum()*coeff_1_user['PCV']/t_sales
-    vol['Base_sales']= len(data_promo1[data_promo1[hier]==spc_hier]['PCV'])*coeff_1_user['Intercept']/t_sales
+        vol[i]= (data_promo1[data_promo1[hier]==spc_hier][i].sum())*coeff_1_user[0][str('ad_stock_nad_')+str(i)]/t_sales
+    for j in range(4): 
+        vol[str('season_')+str(j)]=(data_promo1[data_promo1[hier]==spc_hier][str('season_')+str(j)].sum())*coeff_1_user[0][str('season_')+str(j)]/t_sales
+    vol['Price']=(data_promo1[data_promo1[hier]==spc_hier]['Price'].sum())*coeff_1_user[0]['Price']/t_sales
+    vol['PCV']=(data_promo1[data_promo1[hier]==spc_hier]['PCV'].sum())*coeff_1_user[0]['PCV']/t_sales
+    vol['Base_sales']= len(data_promo1[data_promo1[hier]==spc_hier]['PCV'])*coeff_1_user[0]['Intercept']/t_sales
     return vol 
 
+
 def rounding_off(data):
-    data={x:round(y,2) for x,y in data.items() if type(y) == float}
+    data={x:round(y,2) for x,y in data.items() if type(y) != str}
+    return data
+
+def rounding_off(data):
+    data={x:round(y,2) for x,y in data.items() if ((type(y) == float) or (type(y)==int)) }
     return data
 #getting coeficients for each brand     #data_promo1
 #creating a dataframe for the coef.    
@@ -52,24 +56,24 @@ def user_input(data_promo1,hier,spc_hier,channel_list,model,lr,decay,config_All_
     for i in range(int(config_All_india_promo[config_All_india_promo['derived_dimension']=='date_var']['num_rav_var'].values[0])):
         date_promo.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='date_var']['rv'+str(i+1)].values[0])
     
-    user_input.last=data_promo1[data_promo1[hier]==spc_hier].tail(1)
+    last=data_promo1[data_promo1[hier]==spc_hier].tail(1)
     #Chann list contain original channel_list no the revised one 
-    user_input.rem_col=list(set(list(user_input.last.columns))-set([hier]+date_promo +['Sales', 'PCV', 'Price']+chann_list+list(range(4))))
+    rem_col=list(set(list(last.columns))-set([hier]+date_promo +['Sales', 'PCV', 'Price']+chann_list+[str('season_')+str(i) for i in range(4)]))
     
-    user_input.last_val=user_input.last.drop(columns=user_input.rem_col)
-    user_input.last_val_dict= user_input.last_val.reindex().to_dict('records')
+    last_val=last.drop(columns=rem_col)
+    last_val_dict= last_val.reindex().to_dict('records')
     
-    user_input.coeff_1=coeff123(data_promo1,hier,spc_hier,model) 
-    user_input.coeff_1_user=user_input.coeff_1.to_dict('records')
+    coeff_1=coeff123(data_promo1,hier,spc_hier,Model.mdf1_sea) 
+    coeff_1_user=coeff_1.to_dict('records') 
     
     #CREATING A VOLUME DISTRIBUTION BAR CHART (CONTRIBUTION) 
     vol_dist=vol_distr(data_promo1,hier,spc_hier,chann_list,coeff_1_user)
     
 # =============================================================================
-#     rounding_off(user_input.last_val_dict)
-#     rounding_off(user_input.coeff_1_user)
+#     rounding_off(last_val_dict)
+#     rounding_off(coeff_1_user)
 # =============================================================================
-    output={'last_param':rounding_off(user_input.last_val_dict[0]),'coeff':rounding_off(user_input.coeff_1_user[0])}
+    output={'last_param':rounding_off(last_val_dict[0]),'coeff':rounding_off(coeff_1_user[0])}
 
     return output
     
