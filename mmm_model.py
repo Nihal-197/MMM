0ae94d0596a34083afbfe0380e38795958edf630
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-27 13:52:18 +0530

    Update mmm_model.py

--- mmm_model.py
+++ mmm_model.py
@@ -6,7 +6,6 @@ from sklearn.metrics import r2_score,mean_squared_error
 def Model(data_promo1,hier,channel_list):
     non_promo_col=np.array(['Price','PCV'])
     chann=np.array(channel_list)
-    #chann=np.array(chann_list)
 # =============================================================================
 #     #Model 1 with nad curve and log log model 
 #     Model.driver1 = [str("ad_stock_nad_")+i for i in chann.astype('object')+ "_log"]+[j for j in non_promo_col.astype('object') + "_log"]
@@ -22,11 +21,14 @@ def Model(data_promo1,hier,channel_list):
     #mdf= Model.mdf1
     
     #MODEL WITH SEASONALITY 
-    Model.driver1_sea = [str("ad_stock_nad_")+i for i in chann.astype('object')]+[j for j in non_promo_col.astype('object')]+[str('season_')+str(i) for i in range(4)]
-    eqn1_sea=' ~ '+' + '.join([i for i in Model.driver1_sea])
-    md1_sea=smf.mixedlm('Sales' + eqn1_sea,data = data_promo1,groups=data_promo1[hier],re_formula=eqn1_sea)
-    Model.mdf1_sea=md1_sea.fit(method='lbfgs')
-    #mdf1_sea = Model.mdf1_sea
+    try :
+        Model.driver1_sea = [str("ad_stock_nad_")+i for i in chann.astype('object')]+[j for j in non_promo_col.astype('object')]+[str('season_')+str(i) for i in range(4)]
+        eqn1_sea=' ~ '+' + '.join([i for i in Model.driver1_sea])
+        md1_sea=smf.mixedlm('Sales' + eqn1_sea,data = data_promo1,groups=data_promo1[hier],re_formula=eqn1_sea)
+        Model.mdf1_sea=md1_sea.fit()
+    except Exception as e:
+        raise Exception("Model with seasons failed to get trained due to "+str(e))
+    #mdf1_sea = Model.mdf1_sea 
     
 # =============================================================================
 #     #Model 2 with S curve 
@@ -35,12 +37,9 @@ def Model(data_promo1,hier,channel_list):
 #     md2=smf.mixedlm('Sales_log' + eqn2,data = data_promo1,groups=data_promo1[hier],re_formula=eqn2)
 #     Model.mdf2=md2.fit(method='cg')
 # =============================================================================
-     
-    Model.driver=Model.driver1
-    Model.mdf=Model.mdf1
     
-    rms1 = r2_score(data_promo1['Sales'],Model.mdf1.fittedvalues)
-    rms2 = r2_score(data_promo1['Sales'],Model.mdf1_sea.fittedvalues)
+    Model.acc1 = r2_score(data_promo1['Sales'],Model.mdf1.fittedvalues)
+    Model.acc2 = r2_score(data_promo1['Sales'],Model.mdf1_sea.fittedvalues)
 
 # =============================================================================
 #     
