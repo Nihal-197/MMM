d2bd8d5a1cb363476b98461436ef1c7bb146a1a2
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-27 13:52:01 +0530

    Update mmm_pre_zone.py

--- mmm_pre_zone.py
+++ mmm_pre_zone.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import re
 import itertools
+from corr_finder import *
 
 def log_var_crores(data,var):
     data[var+str('_log')]=np.log(data[var])
@@ -11,10 +12,6 @@ def ad_stock_s_curve_u(data,data_promo,var,hier,spc_hier,zone_reg_col,lr_list,de
     
     lr_rate=float((lr_list[var]))
     decay=float(decay_list[var]) 
-
-# =============================================================================
-#             testing the data, delete the variable's values
-# =============================================================================
 
     if n==2:
         a=[]
@@ -134,20 +131,17 @@ def filling_na(data,hier,zone_list,channel_list):
                 data.loc[index,promo]=spend_promo
 def pre1(test_data_all,data_promo,config_All_india_HFD,config_All_india_promo,hier,spc_hier,cor_coef_ad=0.7,cor_coef_else=0.8):
 #for brand drop subbrand, for manuf. drop band and subbrand 
-    hier= MMM1.hier
-    spc_hier = MMM1.spc_hier
-    cor_coef_ad=0.9
-    cor_coef_else=0.8
-    hier_list = []
-    for i in range(config_All_india_HFD[config_All_india_HFD['derived_dimension']=='target_dim']['num_rav_var'].sum()):
-        hier_list.append(config_All_india_HFD[config_All_india_HFD['derived_dimension']=='target_dim']['rv'+str(i+1)].sum())
-    date_HFD=[]
-    for i in range(config_All_india_HFD[config_All_india_HFD['derived_dimension']=='date_var']['num_rav_var'].values[0]):
-        date_HFD.append(config_All_india_HFD[config_All_india_HFD['derived_dimension']=='date_var']['rv'+str(i+1)].values[0])
 
-    date_promo=[]
+    pre1.hier_list = []
+    for i in range(config_All_india_HFD[config_All_india_HFD['derived_dimension']=='target_dim']['num_rav_var'].sum()):
+        pre1.hier_list.append(config_All_india_HFD[config_All_india_HFD['derived_dimension']=='target_dim']['rv'+str(i+1)].sum())
+    pre1.date_HFD=[]
+    for i in range(config_All_india_HFD[config_All_india_HFD['derived_dimension']=='date_var']['num_rav_var'].values[0]):
+        pre1.date_HFD.append(config_All_india_HFD[config_All_india_HFD['derived_dimension']=='date_var']['rv'+str(i+1)].values[0])
+
+    pre1.date_promo=[]
     for i in range(int(config_All_india_promo[config_All_india_promo['derived_dimension']=='date_var']['num_rav_var'].values[0])):
-        date_promo.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='date_var']['rv'+str(i+1)].values[0])
+        pre1.date_promo.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='date_var']['rv'+str(i+1)].values[0])
 
     #creating a Price column
     p_config=config_All_india_promo[config_All_india_promo['derived_dimension']=='Price']
@@ -158,37 +152,37 @@ def pre1(test_data_all,data_promo,config_All_india_HFD,config_All_india_promo,hi
     a=test_data_all.rename(columns=a_dict).eval(p_config['formula'])
     test_data_all['Price']=a[0]
     
-    test_data_all.drop(columns=col_drop(hier,hier_list),inplace=True)
+    test_data_all.drop(columns=col_drop(hier,pre1.hier_list),inplace=True)
 
     #SPENDS=config_All_india_promo[config_All_india_promo['derived_dimension']=='Spends']['rv'+str(1)].values[0]
     PCV=config_All_india_promo[config_All_india_promo['derived_dimension']=='PCV']['rv'+str(1)].values[0]
     SALES=config_All_india_promo[config_All_india_promo['derived_dimension']=='Sales']['rv'+str(1)].values[0]
-    zone_reg_col=[] 
+    pre1.zone_reg_col=[] 
     for i in range(2):
-            zone_reg_col.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='geo_level']['rv'+str(i+1)].values[0])
+            pre1.zone_reg_col.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='geo_level']['rv'+str(i+1)].values[0])
 
     #Below is the code if config file contain the column names of the spends as different columns
-    channel_list=[]
+    pre1.channel_list=[]
     for i in range(int((config_All_india_promo[config_All_india_promo['derived_dimension']=='promotion']['num_rav_var'].sum()))):
-        channel_list.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='promotion']['rv'+str(i+1)].sum())
+        pre1.channel_list.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='promotion']['rv'+str(i+1)].sum())
 
-    lr={}
-    decay={}
+    pre1.lr={}
+    pre1.decay={}
     for i in range(config_All_india_promo[config_All_india_promo['derived_dimension']=='Learning Rates']['num_rav_var'].sum()):
-        lr[channel_list[i]]=config_All_india_promo[config_All_india_promo['derived_dimension']=='Learning Rates']['rv'+str(i+1)].sum()
-        decay[channel_list[i]]=config_All_india_promo[config_All_india_promo['derived_dimension']=='Decay Rates']['rv'+str(i+1)].sum()
+        pre1.lr[pre1.channel_list[i]]=config_All_india_promo[config_All_india_promo['derived_dimension']=='Learning Rates']['rv'+str(i+1)].sum()
+        pre1.decay[pre1.channel_list[i]]=config_All_india_promo[config_All_india_promo['derived_dimension']=='Decay Rates']['rv'+str(i+1)].sum()
 
     #treating outliers in the model removing the values over 99%
-    filling_na(data_promo,hier_list[2],zone_reg_col,channel_list)
+    filling_na(data_promo,pre1.hier_list[2],pre1.zone_reg_col,pre1.channel_list)
 
-    list_d_hier=list(date_promo)+hier_list[0:3]+zone_reg_col#list containing [Month,Manufacture,Brand,Subbrand,Zone,Region]
-    data_promo_2=data_promo.groupby(by=list(set(list_d_hier)-set(col_drop(hier,hier_list)))).sum().reset_index()
-    data_promo1= pd.merge(test_data_all,data_promo_2, on =list(set(list_d_hier)-set(list(col_drop(hier,hier_list)))),how='left')
+    list_d_hier=list(pre1.date_promo)+pre1.hier_list[0:3]+pre1.zone_reg_col#list containing [Month,Manufacture,Brand,Subbrand,Zone,Region]
+    data_promo_2=data_promo.groupby(by=list(set(list_d_hier)-set(col_drop(hier,pre1.hier_list)))).sum().reset_index()
+    data_promo1= pd.merge(test_data_all,data_promo_2, on =list(set(list_d_hier)-set(list(col_drop(hier,pre1.hier_list)))),how='left')
     data_promo1.rename(columns={SALES:'Sales',PCV:'PCV'},inplace=True)
 
     #columns not to drop(non promo,sales,promo(nad stock),date)
     non_promo_col=['Price','PCV']
-    non_drop_col=list(date_promo)+[hier]+list(channel_list)+['Sales']+non_promo_col + zone_reg_col
+    non_drop_col=list(pre1.date_promo)+[hier]+list(pre1.channel_list)+['Sales']+non_promo_col + pre1.zone_reg_col
    
     #replacing any nan due to division for taking log later
     mean=data_promo1['Price'].mean()
@@ -197,14 +191,14 @@ def pre1(test_data_all,data_promo,config_All_india_HFD,config_All_india_promo,hi
     #a=zeroes_finder(data_promo1)
     rep_zero(data_promo1,'PCV')
 
-    spc_hier_list = list(data_promo1[hier].unique())
+    pre1.spc_hier_list = list(data_promo1[hier].unique())
      #droppping other columns 
     data_promo1.drop(columns=list(set(list(data_promo1.columns))-set(non_drop_col)),inplace=True)
 
     
-    for i in (channel_list):
-        for j in spc_hier_list: 
-            ad_stock_s_curve_u(data_promo1,data_promo,i,hier,j,zone_reg_col,lr,decay)
+    for i in (pre1.channel_list):
+        for j in pre1.spc_hier_list: 
+            ad_stock_s_curve_u(data_promo1,data_promo,i,hier,j,pre1.zone_reg_col,pre1.lr,pre1.decay)
 # =============================================================================
 #             log_var_crores(data_promo1,str('ad_stock_nad_')+i)
 #             log_var_crores(data_promo1,str('ad_stock_s_')+i)
@@ -218,7 +212,7 @@ def pre1(test_data_all,data_promo,config_All_india_HFD,config_All_india_promo,hi
     #CHECK FOR CORRELATION AND RELATED CHANGES 
     
     #GET THE LIST OF COLUMNS TO TAKE CORRELATION MATRIX FOR 
-    chann=np.array(channel_list)   
+    chann=np.array(pre1.channel_list)   
     non_promo_col=np.array(['Price','PCV'])
     driver_col=[str("ad_stock_nad_")+i for i in chann.astype('object')]+[j for j in non_promo_col.astype('object') ]
     lis=driver_col+['Sales']
@@ -226,19 +220,20 @@ def pre1(test_data_all,data_promo,config_All_india_HFD,config_All_india_promo,hi
     #CORRELATION MATRIX 
     corr= data_promo1[lis].corr() 
      
-    cor1_dict= corr_find(data_promo1,channel_list,cor_coef_ad,cor_coef_else)
+    cor1_dict= corr_find(data_promo1,pre1.channel_list,cor_coef_ad,cor_coef_else)
     #get the mapped dictionary ex. 'Digital' : 'ad_stock_nad_Digital_log
     mapped=new_map_dict(corr_find.corr) 
-    
-    data_promo1,chann_list,added_col1,added_col2=corr_merge_zone(data_promo,data_promo1,hier,spc_hier,zone_reg_col,channel_list,spc_hier_list,mapped,cor1_dict,cor_coef_ad,cor_coef_else,lr,decay)
+    pre1.added_col1=deque([]) 
+    pre1.added_col2=deque([])
+    data_promo1,pre1.chann_list,pre1.added_col1,pre1.added_col2=corr_merge_zone(pre1.added_col1,pre1.added_col2,data_promo,data_promo1,hier,spc_hier,pre1.zone_reg_col,pre1.channel_list,pre1.spc_hier_list,mapped,cor1_dict,cor_coef_ad,cor_coef_else,pre1.lr,pre1.decay)
     #we get return as df, new channel_list and the list of exchanges of columns in deque
     
     #GET DUMMIES FOR SEASONALITY 
-    data_promo1[date_promo[0]]=data_promo1[date_promo[0]].dt.month
+    data_promo1[pre1.date_promo[0]]=data_promo1[pre1.date_promo[0]].dt.month
     #CREATING 4 BINS 
-    data_promo1[date_promo[0]]=pd.cut(data_promo1[date_promo[0]],4,labels=[str('season_')+str(i) for i in range(4)])
+    data_promo1[pre1.date_promo[0]]=pd.cut(data_promo1[pre1.date_promo[0]],4,labels=[str('season_')+str(i) for i in range(4)])
     #CREATING DUMMY VARIABLES 
-    daa=pd.get_dummies(data_promo1[date_promo[0]])
+    daa=pd.get_dummies(data_promo1[pre1.date_promo[0]])
     #JOIN BOTH
     data_promo1 = data_promo1.join(daa)
     
