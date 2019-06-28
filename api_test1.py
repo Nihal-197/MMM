33bc96b33d53e8d6c3d04a7ca1b6084fb6481624
Nihal Singh <31930532+Nihal-197@users.noreply.github.com>
2019-06-28 14:51:40 +0530

    first version

--- api_test1.py
+++ api_test1.py
@@ -15,6 +15,17 @@ warnings.filterwarnings('ignore')
 #==============================================================================
 #MMM
 def MMM1(data_HFD,data_promo,config_All_india_HFD,config_All_india_promo,data_json):
+    
+    data_json={ 
+        "api_key":456,
+        "api_secret": 456,
+        "hier": "Brand",
+        "spc_hier": "COMPLAN",
+        "mod": "Zone", 
+        "zone": "SOUTH",
+        "region": "RURAL"
+        
+    }
     #==========================================================================
     #Raising error in the data provided itself
     #then checking for the columns in the pre_processing module
@@ -61,94 +72,97 @@ def MMM1(data_HFD,data_promo,config_All_india_HFD,config_All_india_promo,data_js
         MMM1.zone =data_json['zone'].capitalize()
         MMM1.region=data_json['region'].capitalize()
         
-        data_validator(data_promo,'Zone',MMM1.zone)
-        data_validator(data_promo,'Region',MMM1.region)
+# =============================================================================
+#         data_validator(data_promo,'Zone',MMM1.zone)
+#         data_validator(data_promo,'Region',MMM1.region)
+# =============================================================================
         
         #==========================================================================
         #FILTER FOR ZONAL DATA 
         test_data_all=data_HFD[(data_HFD['Level']==MMM1.hier)&(data_HFD['Level_Geo']=='Zone')]
         test_data_all=test_data_all[(test_data_all['Zone']==MMM1.zone)&(test_data_all['Region']==MMM1.region)].reset_index(drop=True)
         
-        
-        #THIS FILTER IS ALREADY PROVIDED WITH EQL
-        channel_list=[]
-        for i in range(int((config_All_india_promo[config_All_india_promo['derived_dimension']=='promotion']['num_rav_var'].sum()))):
-            channel_list.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='promotion']['rv'+str(i+1)].sum())
-
-        #This filter is already provided with EQL
-        test_data_all=test_data_all[['Zone', 'Region','Month', 'Sales Volume (Volume(LITRES))','WD PCV Handling%','Value Offtake(000 Rs)']+[hier]]
-        data_promo=data_promo[['Zone', 'Region','Month']+[MMM1.hier]+channel_list]
-        
-
+# =============================================================================
+#         #This filter is already provided with EQL
+#         test_data_all=test_data_all[['Zone', 'Region','Month', 'Sales Volume (Volume(LITRES))','WD PCV Handling%','Value Offtake(000 Rs)']+[MMM1.hier]]
+#         data_promo=data_promo[['Zone', 'Region','Month']+[MMM1.hier]+channel_list]
+#         
+# =============================================================================
         #==========================================================================
     
         #=========================PREPROCESSING====================================
-        MMM1.data_promo1=pre1(test_data_all,data_promo,config_All_india_HFD,config_All_india_promo,MMM1.hier,MMM1.spc_hier)
+        MMM1.data_promo1=pre1(test_data_all,data_promo,config_All_india_HFD,config_All_india_promo,MMM1.hier,MMM1.spc_hier,0.89,0.8)
         print('Pre processing done for Zone')
+        
+        #==============================================================================
+        #correlation finder for 2 different curves 
+        #corr_fin(data_promo1,pre.channel_list)
+        #==============================================================================
+        
         #=============================MODEL============================================
-        Model(MMM1.data_promo1,MMM1.hier,pre1.channel_list)
+        Model(MMM1.data_promo1,MMM1.hier,pre1.chann_list)
         print('Model trained')
+        print(f'R-2 score :{Model.acc1}')
+        print(f'R-2 score with seasons : {Model.acc2}')
         #========================USER_INPUT============================================
-        final_data1=user_input(MMM1.data_promo1,MMM1.hier,MMM1.spc_hier,pre1.channel_list,Model.mdf,pre1.lr,pre1.decay,config_All_india_promo,Model.driver,data_json)
+        final_data1=user_input(MMM1.data_promo1,MMM1.hier,MMM1.spc_hier,pre1.channel_list,Model.mdf1_sea,pre1.lr,pre1.decay,config_All_india_promo,data_json,pre1.chann_list,pre1.added_col1,pre1.added_col2)
         MMM1.channel_list= pre1.channel_list.copy()
         MMM1.lr= pre1.lr.copy()
-        MMM1.decay = pre1.decay.copy()
-        MMM1.mdf= Model.mdf
-        MMM1.driver=Model.driver
+        MMM1.decay = pre1.decay.copy() 
+        MMM1.mdf1= Model.mdf1
+        MMM1.mdf1_sea= Model.mdf1_sea
+        MMM1.driver1=Model.driver1
+        MMM1.chann_list = pre1.chann_list
         return final_data1
     #==============================================================================
-    elif(MMM1.mod=='Allindia' or MMM1.mod=='allindia' or MMM1.mod=='All india' or MMM1.mod=='AllIndia' or MMM1.mod=='all india'):
+    if(MMM1.mod=='Allindia' or MMM1.mod=='allindia' or MMM1.mod=='All india' or MMM1.mod=='AllIndia' or MMM1.mod=='all india'):
         
         #==========================================================================
         #FILTERING ALL INDIA DATA 
         test_data_all=data_HFD[(data_HFD['Level']==MMM1.hier)&(data_HFD['Level_Geo']=='Country')] #changes made(changed from country == all india)
         #==========================================================================
-        
-        #THIS FILTER IS ALREADY PROVIDED WITH EQL
-        channel_list=[]
-        for i in range(int((config_All_india_promo[config_All_india_promo['derived_dimension']=='promotion']['num_rav_var'].sum()))):
-            channel_list.append(config_All_india_promo[config_All_india_promo['derived_dimension']=='promotion']['rv'+str(i+1)].sum())
-            
-        #This filter is already provided with EQL            
-        test_data_all=test_data_all[['Month', 'Sales Volume (Volume(LITRES))','WD PCV Handling%','Value Offtake(000 Rs)']+[hier]]
-        data_promo=data_promo[['Month']+[MMM1.hier]+channel_list]
-        
+  # =============================================================================
+#         #This filter is already provided with EQL            
+#         test_data_all=test_data_all[['Month', 'Sales Volume (Volume(LITRES))','WD PCV Handling%','Value Offtake(000 Rs)']+[MMM1.hier]]
+#         data_promo=data_promo[['Month']+[MMM1.hier]+channel_list]
+#         
+# =============================================================================
 
         #=========================PREPROCESSING====================================
         MMM1.data_promo1=pre2(test_data_all,data_promo,config_All_india_HFD,config_All_india_promo,MMM1.hier,MMM1.spc_hier)
         #==========================================================================
         print('Pre processing done for AllIndia')
-            #=============================MODEL============================================
-        Model(MMM1.data_promo1,MMM1.hier,pre2.channel_list)
+        
+        #==============================================================================
+        #correlation finder for 2 different curves 
+        #corr_fin(data_promo1,pre.channel_list)
+        #==============================================================================
+        
+        #=============================MODEL============================================
+        Model(MMM1.data_promo1,MMM1.hier,pre2.chan_list)
         print('Model trained')
         #========================USER_INPUT============================================
         final_data1=user_input(MMM1.data_promo1,MMM1.hier,MMM1.spc_hier,pre2.channel_list,Model.mdf,pre2.lr,pre2.decay,config_All_india_promo,Model.driver,data_json)
         MMM1.channel_list= pre2.channel_list.copy()
         MMM1.lr= pre2.lr.copy()
         MMM1.decay = pre2.decay.copy()
-        MMM1.mdf= Model.mdf
-        MMM1.driver=Model.driver
+        MMM1.mdf1= Model.mdf1
+        MMM1.mdf1_sea= Model.mdf1_sea
+        MMM1.driver1=Model.driver1
+        MMM1.driver1_sea=Model.driver1_sea
+        MMM1.chann_list = pre2.chann_list
         return final_data1
-    #==============================================================================
-    #correlation finder for 2 different curves 
-    #corr_fin(data_promo1,pre.channel_list)
-    #==============================================================================
-    
-
         #Saving few variables 
-
     #==============================================================================
     #create a post_processing part 2 for taking next few inputs
-def final(data_promo1,hier,spc_hier,channel_list,mdf,lr,decay,config_All_india_promo,driver,data_json):
+def final(data_promo1,hier,spc_hier,channel_list,chann_list,driver1_sea,driver1,mdf1_sea,lr,decay,config_All_india_promo,driver,data_json,mod):
     
     #function just for checking if it contains the values 
     check_input(data_json,['api_key','api_secret']+[i for i in channel_list]+['PCV','Price'])
     
     #check if the PCV has value between  0-100 as its a percentage and the rest of the values cannot be negative
-    non_neg_val(data_json,channel_list)
-    
-    
-    
-    final_data2=user_input_part2(data_promo1,hier,spc_hier,channel_list,mdf,lr,decay,config_All_india_promo,driver,data_json)
+    non_neg_val(data_json,channel_list) 
+    #data_promo1,hier,spc_hier,channel_list,chann_list,driver1_sea,driver1,mdf1_sea,lr,decay,config_All_india_promo,driver,data_json,mod
+    final_data2=user_input_part2(data_promo1,hier,spc_hier,channel_list,chann_list,driver1_sea,driver1,mdf1_sea,lr,decay,config_All_india_promo,driver,data_json,mod)
     return final_data2
     
